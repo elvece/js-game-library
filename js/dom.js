4192dbbeee6b72ffc4ca7b546fbece2f80768ed8
elvece <lvcifferello@gmail.com>
2015-08-11 16:36:53 -0600

    added game constructor and render mehtod; added game library constructor, addGames and render methods; created some game isntances

---
+++ js/dom.js
@@ -0,0 +1,7 @@
+$(document).on('ready', function() {
+  $('body')
+    .append(catan.render())
+    .append(cardGames.render());
+
+});
+
--- js/main.js
+++ js/main.js
@@ -1,5 +1,50 @@
-// add scripts
 
-$(document).on('ready', function() {
-  console.log('sanity check!');
-});
+//Game constructor
+function Game (title, genre){
+  this.title = title;
+  this.genre = genre;
+}
+
+//adds game to the DOM
+Game.prototype.render = function() {
+  this.$element = $('<div>')
+    .append('<strong>Game</strong>: ' + this.title)
+    .append('<br>')
+    .append('<strong>Genre</strong>: ' + this.genre)
+    .addClass('col-md-6');
+  return this.$element;
+};
+
+function GameLibrary (title, games) {
+  this.title = title;
+  this.games = [];
+}
+
+GameLibrary.prototype.addGames = function(instance){
+  return this.games.push(instance);
+};
+
+GameLibrary.prototype.render = function() {
+  this.$element = $('<div>')
+    .append('<h3>' + this.title + '</h3>')
+    .append('<br>')
+    .append(this.games)
+    .addClass('col-md-6');
+    return this.$element;
+};
+
+//game instances
+var catan = new Game ("Settlers of Catan", "Board Games");
+var chess = new Game ("Chess", "Board Games");
+var cards = new Game("Cards Against Humanity", "Card Games");
+var rummy = new Game("Rummy", "Card Games");
+var poker = new Game("Poker", "Card Games");
+var wow = new Game("World of Warcraft", "Computer Games");
+var minecraft = new Game("Minecraft", "Computer Games");
+
+//add games
+addGames(cards);
+
+//game libraries
+var cardGames = new GameLibrary ("Card Games");
+
